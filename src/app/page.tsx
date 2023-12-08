8575d050f189cbbe8c54c5e876c0552f1187dfde
Tushar Goyal <tushar462024@gmail.com>
2023-12-08 17:16:57 +0530

    Remove old files

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@nextui-org/button";
-
-export default function Page() {
-  return (
-    <div className="flex flex-grow justify-center items-center h-screen">
-      <Button>Click me</Button>
-    </div>
-  );
-}
